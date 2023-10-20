e652a46a35138ba6d9a887d35628a9044e15c1f2
jacklee.jabra@yahoo.com <evilc4tofficial@gmail.com>
2023-10-20 12:17:45 -0400

    Added puppeteerrc config file and reinstalled puppeteer

--- index.js
+++ index.js
@@ -71,12 +71,12 @@ app.post('/form', async (req, res) => {
             
         // }
 
-        await page.waitForSelector('img', { timeout: 5_000 });
-        // await page.waitForSelector('video', { timeout: 5_000 });
-        await page.waitForSelector('p', { timeout: 5_000 });
-        await page.waitForSelector('a', { timeout: 5_000 });
-        await page.waitForSelector('script', { timeout: 5_000 });
-        await page.waitForSelector('link', { timeout: 5_000 });
+        // await page.waitForSelector('img', { timeout: 5_000 });
+        // // await page.waitForSelector('video', { timeout: 5_000 });
+        // await page.waitForSelector('p', { timeout: 5_000 });
+        // await page.waitForSelector('a', { timeout: 5_000 });
+        // await page.waitForSelector('script', { timeout: 5_000 });
+        // await page.waitForSelector('link', { timeout: 5_000 });
         page.waitForNetworkIdle()    
 
         await page.emulateMediaType('screen');
